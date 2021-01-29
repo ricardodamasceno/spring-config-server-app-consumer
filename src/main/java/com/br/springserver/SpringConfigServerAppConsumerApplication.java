7ed4046aedb3514bc61cdb8760bf9f9ddc7aacfa
Ricardo Damasceno <ricardodamascenno@gmail.com>
2021-01-29 15:50:59 -0300

    Addition of mysql configuration

---
+++ src/main/java/com/br/springserver/SpringConfigServerAppConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.br.springserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConfigServerAppConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringConfigServerAppConsumerApplication.class, args);
+    }
+
+}
